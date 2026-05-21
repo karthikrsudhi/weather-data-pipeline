1d6ba5016d357134f034ef50ecac5e2d1ba49a4e
Karthik <karthik@email.com>
2026-05-21 20:30:26 +0530

    Added weather data upload to Amazon S3

--- lambda_dynamodb_to_s3.py
+++ lambda_dynamodb_to_s3.py
@@ -18,6 +18,12 @@ def lambda_handler(event, context):
 
 file_name = f"{item['city']}_{item['time']}.json"
 
+s3.put_object(
+    Bucket=BUCKET_NAME,
+    Key=file_name,
+    Body=json.dumps(item)
+)
+
 
 
 
